8976cda1d0f8d65e3dd71b5ccbf1179498df896e
Varshannam05 <166928794+Varshannam05@users.noreply.github.com>
2025-05-26 11:31:53 +0530

    Delete module1/html directory

--- module1/html/js/main.js
+++
@@ -1,9 +0,0 @@
-function initPage() {
-  console.log("Welcome to the Community Portal");
-  alert("Page fully loaded");
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("registerBtn");
-  btn.onclick = () => alert("Registration successful!");
-});
